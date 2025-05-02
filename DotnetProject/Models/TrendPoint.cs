0cd5491690fcd5ee67bc5f4d5f7c37e83a14135f
Csongor Bőjte <bojte.csongi12@gmail.com>
2025-05-02 13:36:42 +0300

    Update trend visualization

--- DotnetProject/Models/TrendPoint.cs
+++ DotnetProject/Models/TrendPoint.cs
@@ -3,7 +3,6 @@ namespace DotnetProject.Models;
 public class TrendPoint
 {
     public int Year { get; set; }
-    public string YearString => Year.ToString();
-
+    public string YearString { get; set; }
     public double LastMadm { get; set; }
 }
